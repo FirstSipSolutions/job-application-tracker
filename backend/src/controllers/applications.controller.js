ac439fb2a566a076b31c5544db5a2785eb15f930
Chris <Britten63@hotmail.com>
2026-03-09 01:53:57 -0300

    feat: createApplication POST endpoint

--- backend/src/controllers/applications.controller.js
+++ backend/src/controllers/applications.controller.js
@@ -37,13 +37,33 @@ const getAllApplications = async (req, res, next) => {
 const createApplication = async (req, res, next) => {
   try {
     // TODO: Validate with applicationSchema.safeParse() -- this is done!
-    // TODO: INSERT and return new row with 201 - in progress now
+    // TODO: INSERT and return new row with 201 - this is done!
 
     const parsed = applicationSchema.safeParse(req.body);
 
     if (!parsed.success) {
       return res.status(400).json({ errors: parsed.error.errors });
     }
+
+    // here is the insert to application
+    // the dollar sign stuff is a secruity habit
+    // this all using zod - pdf in docs to explain better
+
+    const result = await pool.query(
+      `INSERT INTO applications (company_name, job_title, job_url, date_applied, status, notes)
+   VALUES ($1, $2, $3, $4, $5, $6)
+   RETURNING *`,
+      [
+        parsed.data.company_name,
+        parsed.data.job_title,
+        parsed.data.job_url,
+        parsed.data.date_applied,
+        parsed.data.status,
+        parsed.data.notes,
+      ],
+    );
+
+    res.status(201).json(result.rows[0]);
   } catch (err) {
     next(err);
   }
